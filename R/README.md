165d715615b6f60997caf15bf158a4ea17596319
cayetanobv <cayetano.benavent@geographica.gs>
2015-09-04 18:09:58 +0200

    courses resources - R scripts (QGIS) examples

---
+++ R/README.md
@@ -0,0 +1,10 @@
+# R scripts (QGIS) examples)
+Several R scripts for QGIS geoprocessing:
+- Raster histogram (with breaks definition).
+- Raster histogram (with breaks definition and density line).
+- ...
+
+## About author
+Developed by Cayetano Benavent 2015.
+
+http://www.geographica.gs
---
+++ R/Raster_histogram_classes_density.R
@@ -0,0 +1,12 @@
+##Cayetano scripts=group
+##Layer=raster
+##Title=string Histogram of Raster Layer
+##Breaks=number 10
+##showplots
+
+brks <- as.integer(Breaks)
+lyr <- as.matrix(Layer)
+clr <- "lightgreen"
+
+hist(lyr,breaks=brks,main=Title,xlab="Layer",col=clr,prob=TRUE)
+lines(density(lyr),col="blue")
